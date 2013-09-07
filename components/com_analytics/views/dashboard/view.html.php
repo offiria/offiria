2a9b2fafba9b54e58444af37961169440b500f52
Azrul Rahim <write2me@azrul.com>
2013-09-07 10:59:55 +0800

    Clearer copyright notice

--- components/com_analytics/views/dashboard/view.html.php
+++ components/com_analytics/views/dashboard/view.html.php
@@ -2,7 +2,7 @@
 /**
  * @version     1.0.0
  * @package     com_administrator
- * @copyright   Copyright (C) 2011. All rights reserved.
+ * @copyright   Copyright (C) 2011 - 2013 Slashes & Dots Sdn Bhd. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  * @author      Created by com_combuilder - http://www.notwebdesign.com
  */
