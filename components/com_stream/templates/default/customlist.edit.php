49858dbd0ea23a91080990fce69dbe78dd75ea38
Jan Urva <sauanimal@users.noreply.github.com>
2013-09-11 23:18:29 +0300

    Update customlist.edit.php
    Added language translations

--- components/com_stream/templates/default/customlist.edit.php
+++ components/com_stream/templates/default/customlist.edit.php
@@ -1,7 +1,14 @@
+<?php
+if ($customList->id)
+	$title = JText::sprintf('COM_STREAM_LABEL_CUSTOMLIST_EDIT');
+else {
+	$title = JText::sprintf('COM_STREAM_LABEL_CUSTOMLIST_NEW');
+}
+?>
 <div class="modal customlist-modal" id="customlist-edit-message-modal" style="display: none;">
 	<div class="modal-header">
 		<a class="close" data-dismiss="modal">×</a>
-		<h3><?php echo JText::_('COM_TEMPLATE_CUSTOMLIST'); ?></h3>
+		<h3><?php echo $title; ?></h3>
 	</div>
 	<form method="post" id="customlist-form" action="<?php echo JRoute::_('index.php?option=com_stream&view=customlist&task=save'); ?>">
 		<div class="modal-body">
@@ -14,8 +21,8 @@
 			</div>
 		</div>
 		<div class="modal-footer">
-			<a class="btn" data-dismiss="modal">Cancel</a>
-			<input type="submit" class="btn btn-primary " name="btnSubmit" value="<?php echo JText::_('Continue'); ?>">
+			<a class="btn" data-dismiss="modal"><?php echo JText::_('COM_STREAM_LABEL_CANCEL'); ?></a>
+			<input type="submit" class="btn btn-primary " name="btnSubmit" value="<?php echo JText::_('COM_STREAM_LABEL_SAVE'); ?>">
 			<input name="customlist_id" type="hidden" value="<?php echo $customList->id; ?>">
 		</div>
 	</form>
