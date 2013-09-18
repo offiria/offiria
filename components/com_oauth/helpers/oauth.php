3258b481490afb1d15ea9f0f76bd317d8fd338b4
Azrul Rahim <write2me@azrul.com>
2013-09-18 11:59:17 +0800

    Update author header

--- components/com_oauth/helpers/oauth.php
+++ components/com_oauth/helpers/oauth.php
@@ -4,7 +4,7 @@
  * @package     com_oauth
  * @copyright   Copyright (C) 2011 - 2013 Slashes & Dots Sdn Bhd. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
- * @author      Created by com_combuilder - http://www.notwebdesign.com
+ * @author      Offiria Team
  */
 
 abstract class OauthHelper
--- components/com_scripts/scripts.php
+++ components/com_scripts/scripts.php
@@ -4,7 +4,7 @@
  * @package     com_Stream
  * @copyright   Copyright (C) 2011. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
- * @author      Created by com_combuilder - http://www.notwebdesign.com
+ * @author      Offiria Team
  */
 
 defined('_JEXEC') or die;
